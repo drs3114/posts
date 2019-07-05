7ad048fb9bd30d9f8782d410532b230f9bece761
Deepak Shankar <shankar.deepak22@gmail.com>
2019-07-05 17:12:44 -0400

    update posts model by associating a type

--- src/main/java/com/deepakshankar/posts/model/PostType.java
+++ src/main/java/com/deepakshankar/posts/model/PostType.java
@@ -1,4 +1,15 @@
 package com.deepakshankar.posts.model;
 
 public enum PostType {
+    BLOG(0,"BLOG", "A blog post"),
+    POEM(1,"POEM","A poem post");
+    public int id;
+    public String name;
+    public String description;
+
+    PostType(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 }
