2096272af3624284df3fcfb7f191d9085eea4f14
Deepak Shankar <shankar.deepak22@gmail.com>
2019-07-20 22:04:43 -0400

    working copy

--- posts-service/src/main/java/com/deepakshankar/posts/PostsApplication.java
+++ posts-service/src/main/java/com/deepakshankar/posts/PostsApplication.java
@@ -2,31 +2,9 @@ package com.deepakshankar.posts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-/*@EnableSwagger2*/
+@SpringBootApplication
 public class PostsApplication {
-
-    /*@Bean
-    public Docket api() {
-        return new Docket(SWAGGER_2)
-                .select()
-                .apis(any())
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }*/
-
-    /*private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "Posts Rest API",
-                "REST API documentation for Posts backend service",
-                "Snapshot",
-                "Terms of service",
-                new Contact("Deepak Shankar", "https://deepakshankar.com", "shankar.deepak22@gmail.com"),
-                "License of API", "API license URL", Collections.emptyList());
-    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(PostsApplication.class, args);
